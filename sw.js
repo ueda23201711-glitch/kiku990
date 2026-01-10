9c4c96c8be15fc4a03902b166c6846c65015acdd
ueda23201711-glitch <ueda23201711@gmail.com>
2026-01-10 14:13:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,32 @@
-const CACHE_NAME="kikutan-toeic990-v4";
-const ASSETS=[
+const CACHE_NAME = "kikutan-toeic990-v1";
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/toeic990-192.png",
-  "./icons/toeic990-512.png",
-  "./icons/toeic990-maskable-192.png",
-  "./icons/toeic990-maskable-512.png",
-  "./icons/apple-touch-icon-180.png"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon-180.png",
+  "./icons/favicon-32.png",
+  "./icons/favicon-16.png"
 ];
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE_NAME?null:caches.delete(k)))).then(()=>self.clients.claim()));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
 });
-self.addEventListener("fetch", e=>{
-  if (e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(cached=>cached || fetch(e.request).then(res=>{
-    const copy=res.clone();
-    caches.open(CACHE_NAME).then(c=>c.put(e.request, copy));
-    return res;
-  })));
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req))
+  );
 });
